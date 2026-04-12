881fb4ce0218e22937046d584b58c012d602508a
Gautam Bansal <bansal.gautam87@gmail.com>
2026-04-12 13:39:45 +0530

    Done

--- myapp/views.py
+++ myapp/views.py
@@ -1,25 +1,3 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
-from .models import *
-from rest_framework import status
-from .serializers import *
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
-@api_view(['GET','POST'])
-def enter_category(request):
-    if request.method == "POST":
-        serializer = categorySerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-class Helloview(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def get(self,request):
-        content = {'message': 'Hello, Greek'}
-        return Response(content)
